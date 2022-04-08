516935c8f418b5854e3087422ec2fe32f857e0fc
dune_geek <dune.geek@gmail.com>
2022-04-08 18:02:01 +0900

    modified myapp.dart but error

--- test/test_OIDC/lib/myapp.dart
+++ test/test_OIDC/lib/myapp.dart
@@ -1,4 +1,6 @@
 //https://auth0.com/blog/get-started-with-flutter-authentication/
+//https://blog.pentagon.tokyo/228/
+//https://auth0.com/blog/flutter-authentication-authorization-with-auth0-part-1-adding-authentication-to-an-app/
 
 import 'package:flutter/material.dart';
 import 'dart:convert';
@@ -46,10 +48,122 @@ class _MyAppState extends State<MyApp> {
 	    title: Text('Auth0 demo'),
 	 ),
 	 body: Center(
-	   child: Text('Imple User AuthN'),
+	   child: isBusy
+              ? CircularProgressIndicator()
+              : isLoggedIn
+                  ? Profile(logoutAction, name, picture)
+                  : Login(loginAction, errorMessage),
 	 ),
        ),
       );
+  }
+
+  Map<String, dynamic> parseIdToken(String idToken) {
+
+     final parts = idToken.split(r'.');
+     assert(parts.length == 3);
+     return jsonDecode(
+        utf8.decode(base64Url.decode(base64Url.normalize(parts[1])))
+     );
+  }
+
+  Future<Map> getUserDetails(String accessToken) async {
+       final url = Uri.parse('https://$AUTH0_DOMAIN/userinfo');
+       final response = await http.get(
+          url,                             // error arg type
+          headers: {'Authorization' : 'bearer $accessToken'}, 
+       );
+
+       if (response.statusCode == 200){
+           return jsonDecode(response.body);
+       }else{
+           throw Exception('Failed to get user details');
+      }
+  }
+  Future<void> loginAction() async {
+    setState(() {
+        isBusy = true;
+        errorMessage = '';
+    });
+    try {
+      final AuthorizationTokenResponse result =
+          await appAuth.authorizeAndExchangeCode(     // error  nullable
+	      AuthorizationTokenRequest(
+		AUTH0_CLIENT_ID,
+		AUTH0_REDIRECT_URI,
+		issuer: 'https://$AUTH0_DOMAIN',
+		scopes: ['openid', 'profile', 'offline_access'],
+		promptValues: ['login']
+	      ),
+           );
+
+      final idToken = parseIdToken(result.idToken);
+      final profile = await getUserDetails(result.accessToken);
+
+      await secureStorage.write(
+          key: 'refresh_token', value: result.refreshToken);
+
+      setState(() {
+        isBusy = false;
+        isLoggedIn = true;
+        name = idToken['name'];
+        picture = profile['picture'];
+      });
+    } catch (e, s) {
+      print('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        isLoggedIn = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  void logoutAction() async {
+     await secureStorage.delete(key: 'refresh_token');
+     setState(() {
+        isLoggedIn = false;
+        isBusy = false;
+     });  
+  }
+
+  @override
+  void initState() {
+       initAction();
+       super.initState();
+  }
+
+  void initAction() async {
+     final storedRefreshToken = await secureStorage.read(key: 'refresh_token');
+     if(storedRefreshToken == null) return;
+
+     setState(() {
+         isBusy = true;
+     });
+     try {
+	 final response = await appAuth.token(TokenRequest(
+	   AUTH0_CLIENT_ID,
+	   AUTH0_REDIRECT_URI,
+	   issuer: AUTH0_ISSUER,
+	   refreshToken: storedRefreshToken,
+	 ));
+
+	 final idToken = parseIdToken(response.idToken);
+	 final profile = await getUserDetails(response.accessToken);
+
+	 secureStorage.write(key: 'refresh_token', value: response.refreshToken);
+
+	 setState(() {
+	   isBusy = false;
+	   isLoggedIn = true;
+	   name = idToken['name'];
+	   picture = profile['picture'];
+	 });
+     } catch (e, s) {
+	 print('error on refresh token: $e - stack: $s');
+	 logoutAction();
+     }
    }
 }
 
